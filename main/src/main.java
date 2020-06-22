60bddafa45bb26e9cb349a19ebf6a88e1f8244d5
Paul VERNIN <paul.vernin@etu.univ-nantes.fr>
2020-06-22 11:13:41 +0200

    4, premier test de la lib

--- main/src/main.java
+++ main/src/main.java
@@ -1,2 +1,10 @@
+import codenames.CodeNamesClient;
+import codenames.exceptions.CnNetworkException;
+
 public class main {
+    public static void main(String[] args) throws CnNetworkException {
+        CodeNamesClient CodenameClient = new CodeNamesClient("http://51.178.49.138:3000/api/v0");
+        System.out.println(CodenameClient.allGames());
+    }
 }
+
