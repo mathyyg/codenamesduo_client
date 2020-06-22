a39b9ee1133d6a796bb5dfbc9a3aaf87790edd6c
Mathys GAGNER <mathys.gagner@etu.univ-nantes.fr>
2020-06-22 18:43:31 +0200

    Demo affichage d'une nouvelle JFrame à partir d'une JFrame

--- main/src/swing/vue/Fenetre.java
+++ main/src/swing/vue/Fenetre.java
@@ -4,6 +4,8 @@ import org.apache.commons.logging.Log;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenetre extends JFrame {
     public Fenetre(String titre) {
@@ -11,6 +13,16 @@ public class Fenetre extends JFrame {
 
         JPanel panelPrincipal = new JPanel();
         this.setContentPane(panelPrincipal);
+
+        JButton suivante = new JButton("Fenêtre suivante");
+
+        panelPrincipal.add(suivante);
+
+        suivante.addActionListener(actionEvent -> {
+            RecherchePan deuxieme = new RecherchePan("suivante");
+            deuxieme.setVisible(true);
+            dispose();
+        });
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
--- main/src/swing/vue/RecherchePan.java
+++ main/src/swing/vue/RecherchePan.java
@@ -1,6 +1,21 @@
 package swing.vue;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class RecherchePan extends JPanel {
+public class RecherchePan extends JFrame {
+    public RecherchePan(String titre) {
+        super(titre);
+        JPanel panelPrincipal = new JPanel();
+        this.setContentPane(panelPrincipal);
+
+        JLabel suivante = new JLabel("Fini");
+
+        panelPrincipal.add(suivante);
+
+        panelPrincipal.setPreferredSize(new Dimension(800, 410));
+
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
 }
