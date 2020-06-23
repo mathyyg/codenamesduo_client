5fddd7ae3c43c7fe9f5b985d8d9f6370a2fa5b2b
Mateo ESTEVES <mateo.esteves@etu.univ-nantes.fr>
2020-06-23 10:18:04 +0200

    Update PartiePan.java

--- main/src/swing/vue/PartiePan.java
+++ main/src/swing/vue/PartiePan.java
@@ -1,8 +1,11 @@
 package swing.vue;
 
-import java.util.List;
-
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
 
 public class PartiePan extends JFrame {
 
@@ -17,7 +20,7 @@ public class PartiePan extends JFrame {
     private JButton nc8 ;
     private JButton nc9 ;
     private JButton nc10 ;
-    private JButton nc11; 
+    private JButton nc11 ; 
     private JButton nc12 ;
     private JButton nc13 ; 
     private JButton nc14 ;
@@ -40,8 +43,73 @@ public class PartiePan extends JFrame {
 
     public PartiePan(String titre) {
         super(titre);
-        JPanel panelPrincipal = new JPanel();
-        this.setContentPane(panelPrincipal);
+        JPanel main = new JPanel();
+        this.setContentPane(main);
+
+        main.setLayout( new GridLayout(2,1));
+
+        JPanel BureauDesLegendes = new JPanel();
+        BureauDesLegendes.setAlignmentX(Component.CENTER_ALIGNMENT);
+        BureauDesLegendes.setAlignmentY(Component.CENTER_ALIGNMENT);
+        BureauDesLegendes.setBorder(BorderFactory.createLineBorder(Color.black));
+        BureauDesLegendes.setBackground(new Color((255),(255),(255)));
+
+
+
+        JPanel plateau = new JPanel();
+        plateau.setBorder(BorderFactory.createRaisedBevelBorder());
+
+        plateau.setLayout(new BoxLayout(plateau, BoxLayout.Y_AXIS));
+        plateau.setBorder(new EmptyBorder(10, 10, 10, 10));
+        JLabel titreplateau = new JLabel("Plateau");
+
+        plateau.add(titreplateau);
+        plateau.add(nc1);
+        plateau.add(nc2);
+        plateau.add(nc3);
+        plateau.add(nc4);
+        plateau.add(nc5);
+        plateau.add(nc6);
+        plateau.add(nc7);
+        plateau.add(nc8);
+        plateau.add(nc9);
+        plateau.add(nc10);
+        plateau.add(nc11);
+        plateau.add(nc12);
+        plateau.add(nc13);
+        plateau.add(nc14);
+        plateau.add(nc15);
+        plateau.add(nc16);
+        plateau.add(nc17);
+        plateau.add(nc18);
+        plateau.add(nc19);
+        plateau.add(nc20);
+        plateau.add(nc21);
+        plateau.add(nc22);
+        plateau.add(nc23);
+        plateau.add(nc24);
+        plateau.add(nc25);
+
+        BureauDesLegendes.add(plateau);
+        main.add(BureauDesLegendes);
+
+        JPanel tips = new JPanel();
+        tips.setBorder(BorderFactory.createRaisedBevelBorder());
+        JLabel titreIndice = new JLabel("Indice");
+
+        JPanel hint = new JPanel();
+        hint.setLayout(new BoxLayout(hint, BoxLayout.Y_AXIS));
+        hint.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        hint.add(titreIndice);
+        hint.add(send);
+        hint.add(indicechiffre);
+        hint.add(indice);
+        hint.add(keycard);
+
+        tips.add(hint);
+        main.add(tips);
+
 
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
