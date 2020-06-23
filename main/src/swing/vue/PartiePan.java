137edd507a0ef7f980ab4c7305f8c5cb572e502e
Mateo ESTEVES <mateo.esteves@etu.univ-nantes.fr>
2020-06-23 09:07:34 +0200

    Update PartiePan.java

--- main/src/swing/vue/PartiePan.java
+++ main/src/swing/vue/PartiePan.java
@@ -1,8 +1,54 @@
 package swing.vue;
 
+import java.util.List;
+
 import javax.swing.*;
 
-public class PartiePan extends JPanel {
+public class PartiePan extends JFrame {
+
+    JPanel BureauDesLegendes ;
+    private JButton nc1 ;
+    private JButton nc2 ;
+    private JButton nc3 ;
+    private JButton nc4 ;
+    private JButton nc5 ;
+    private JButton nc6 ;
+    private JButton nc7 ;
+    private JButton nc8 ;
+    private JButton nc9 ;
+    private JButton nc10 ;
+    private JButton nc11; 
+    private JButton nc12 ;
+    private JButton nc13 ; 
+    private JButton nc14 ;
+    private JButton nc15 ;
+    private JButton nc16 ;
+    private JButton nc17 ; 
+    private JButton nc18 ; 
+    private JButton nc19 ; 
+    private JButton nc20 ;
+    private JButton nc21 ;
+    private JButton nc22 ;
+    private JButton nc23 ;
+    private JButton nc24 ;
+    private JButton nc25 ;
+    private JButton send ;
+    private JButton keycard ;
+    private JTextField indice ;
+    private JComboBox indicechiffre ;
+    protected List cardkey ;
+
+    public PartiePan(String titre) {
+        super(titre);
+        JPanel panelPrincipal = new JPanel();
+        this.setContentPane(panelPrincipal);
+
+
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+
+
 
 
 
