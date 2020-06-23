0e67bc05f7945572e4065ad22b316f04f16dae2f
Paul VERNIN <paul.vernin@etu.univ-nantes.fr>
2020-06-23 16:42:52 +0200

    2.1 : MAJ FenetreLogin

--- main/src/swing/Appli.java
+++ main/src/swing/Appli.java
@@ -11,10 +11,6 @@ public class Appli {
 
         CodeNamesClient serv = new CodeNamesClient("http://51.178.49.138:3000/api/v0");
         FenetreLogin fenetre = new FenetreLogin("Login/Register", serv);
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - fenetre.getWidth()) /2 -250);
-        int y = (int) ((dimension.getHeight() - fenetre.getHeight()) / 2 -150);
-        fenetre.setLocation(x, y);
 
         fenetre.pack();
         fenetre.setVisible(true);
