5dd66ae506a781141a01251cbf574c8ce5900c39
Paul VERNIN <paul.vernin@etu.univ-nantes.fr>
2020-06-24 14:15:36 +0200

    2.6.2 : changement StateListener

--- main/src/swing/timerControleur/StateListener.java
+++ main/src/swing/timerControleur/StateListener.java
@@ -3,6 +3,7 @@ package swing.timerControleur;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import code.Joueur;
 import code.Partie;
 import codenames.CodeNamesClient;
 import codenames.exceptions.CnBadIdException;
@@ -12,18 +13,38 @@ import codenames.states.State;
 import swing.vue.*;
 
 public class StateListener implements ActionListener {
-    FenetrePartie fn;
-    CodeNamesClient serv;
-    Partie partie;
+    private FenetrePartie fn;
+    private CodeNamesClient serv;
+    private Partie partie;
+    boolean majListIndice;
+    private String j1; // Correspond toujours à nous
+    private String j2; // Correspond toujours à notre adversaire
+    private String jDoitRep;
+
     public StateListener(FenetrePartie lafn, Partie lapartie, CodeNamesClient leserv) {
         fn = lafn;
         partie = lapartie;
         serv = leserv;
+        majListIndice = true;
+        try {
+            if (serv.consultGame(partie.getIdPartie()).creator().equals(partie.getJ().getPseudo())){
+                j1 = serv.consultGame(partie.getIdPartie()).creator();
+                j2 = serv.consultGame(partie.getIdPartie()).secondPlayer();
+            } else {
+                j2 = serv.consultGame(partie.getIdPartie()).creator();
+                j1 = serv.consultGame(partie.getIdPartie()).secondPlayer();
+            }
+        } catch (CnNetworkException e) {
+            e.printStackTrace();
+        } catch (CnBadIdException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        boolean majListIndice = true;
         try {
             partie.setEtat(serv.consultGame(partie.getIdPartie()));
             System.out.println("Etat actuel de la partie : "+partie.getEtat().state());
@@ -34,16 +55,19 @@ public class StateListener implements ActionListener {
         }
         if (partie.getEtat().state().equals(STATE_STEP.CLUE_SENT)) {
             try {
-                if (partie.getEtat().clueSender().equals(partie.getJ().login())){
-                    System.out.println(partie.getEtat().clueSender() + "==" + partie.getJ().login());
+                if (partie.getEtat().clueSender().equals(j1)){
+                    System.out.println(partie.getEtat().clueSender() + "==" + j1);
                     System.out.println("vous avez envoyé un indice, ce n'est plus votre tour\nOn attend sa réponse");
                     //On vient d'envoyer l'indice, ce n'est plus notre tour.
+                    jDoitRep = j2;
                 } else {
-                    System.out.println(partie.getEtat().clueSender() + "=/=" + partie.getJ().login());
+                    System.out.println(partie.getEtat().clueSender() + "=/=" + j1);
                     System.out.println("Le joueur adverse a envoyé un indice, c'est à notre tour.\nIl faut choisir une réponse");
                     // récupère la currentClue et currentClueNumber
                     // on doit envoyer une réponse
                     if (majListIndice){
+                        //
+                        jDoitRep = j1;
                         //correspond aux réponses trouvés par l'autre user qu'il faut mettre sur notre plateau.
                         fn.majPreviousAnswer();
                         //ajoute le nouvel indice à la list des indices
@@ -56,17 +80,14 @@ public class StateListener implements ActionListener {
             }
         }
         if (partie.getEtat().state().equals(STATE_STEP.ANSWER_SENT)) {
-            try {
-                if (partie.getEtat().clueSender().equals(partie.getJ().login())){
-                    //on récupe currentAnswer (list des réponses validées par le serv
-                    //on update notre plateau en montrant ce qu'on a trouvé ou non
-                    // ex : 1er mot CODE, 2eme mot : NEUTRAL, 3eme mot : non vérifié du coup
-                    //
-                } else {
-
-                }
-            } catch (IllegalAccessException ex) {
-                ex.printStackTrace();
+            if (j1.equals(jDoitRep)){
+                // on vient d'envoyer la réponse
+                //on récupe currentAnswer (list des réponses validées par le serv
+                //on update notre plateau en montrant ce qu'on a trouvé ou non
+                // ex : 1er mot CODE, 2eme mot : NEUTRAL, 3eme mot : non vérifié du coup
+                //
+            } else {
+                //on fait rien
             }
         }
 
