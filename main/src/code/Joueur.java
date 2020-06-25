e242e2e67e7afb0ea250c16fc852c73bdb8a61ca
Mateo ESTEVES <mateo.esteves@etu.univ-nantes.fr>
2020-06-25 15:29:07 +0200

    v2.14.1 + JAVADOC

--- main/src/code/Joueur.java
+++ main/src/code/Joueur.java
@@ -1,3 +1,8 @@
+/**
+ * @author Les Infopotes
+ * @version 2.14.1
+ */
+
 package code;
 
 import codenames.iPlayer;
@@ -12,14 +17,38 @@ public class Joueur implements iPlayer {
     private String mdp;
     private int partieWin, partieLoos;
 
+
+    /**
+     * Constrcuteur n°1
+     * Constructeur qui permet de saisir que le pseudo
+     * @param lenom String qui correspond au pseudo
+     * @deprecated Utiliser le constructeur n°2
+     */
     public Joueur(String lenom) {
         pseudo = lenom;
         mdp = null;
     }
+
+    /**
+     * Constrcuteur n°2
+     * Constructeur qui permet de saisir le peusdo et le mot de passe
+     * @param lenom String qui correspond au pseudo
+     * @param lemdp String qui coorrespond au mot de passe 
+     */
     public Joueur(String lenom, String lemdp) {
         pseudo = lenom;
         mdp = lemdp;
     }
+
+    /**
+     * Constrcuteur n°3
+     * Constructeur qui permet de saisir toutes les données
+     * @param lenom String qui correspond au pseudo
+     * @param lemdp String qui correspond au mot de passe
+     * @param win Int qui correspond au nombre de partie Gagner par le joueur (s'il change de compte)
+     * @param loos Int qui correspond au nombre de partie Perdu par le joueur (s'il change de compte)
+     * @deprecated Utiliser le constructeur n°2
+     */
     public Joueur(String lenom, String lemdp, int win, int loos) {
         pseudo = lenom;
         mdp = lemdp;
@@ -27,26 +56,87 @@ public class Joueur implements iPlayer {
         partieLoos = loos;
     }
 
+
+    /**
+     * Méthode login
+     * Elle permet de communiquer avec le serveur
+     * @return string qui est le pseudo
+     */
     @Override
     public String login() {
         return pseudo;
     }
 
+
+    /**
+     * Méthode setMdp
+     * Elle permet de définir le mot de passe d'un compte
+     * @param s String qui le mot de passe
+     */
     public void setMdp(String s) {
         mdp = s;
     }
+
+    /**
+     * Méthode qui permet de mettre à jour le nombre de partie gagné par un joueur
+     */
     public void PartieWinUp() { partieWin++;}
+
+    /**
+     * Méthode qui permet de mettre à jour le nombre de partie perdu par un joueur
+     */
     public void PartieLoosUp() { partieLoos++;}
 
+
+    /**
+     * Méthode getMdp
+     * Méthode qui permet de renvoyer le mot de passe du compte
+     * @return string qui est le mot de passe du compte
+     */
     public String getMdp() {
         return mdp;
     }
+
+    /**
+     * Méthode getPseudo
+     * Méthode qui permet de renvoyer le pseudo du compte
+     * @return string qui est le pseudo du compte
+     */
     public String getPseudo() { return pseudo; }
+
+    /**
+     * Méthode getPwin
+     * Elle permet de renvoyer le nombre de partie gagné
+     * @return int qui correspond au nombre de partie Gagné
+     */
     public int getPWin() { return partieWin;}
+
+    /**
+     * Méthode getPloos
+     * Elle permet de renvoyer le nombre de partie Perdu
+     * @return int qui correspond au nombre de partie Perdu
+     */
     public int getPLoos() { return partieLoos;}
+
+    /**
+     * Méthode setPWin 
+     * Elle permet de définir le nombre de partie gagné par le joueur
+     * @param i int qui est mis à jour par PartieWinUp()
+     */
     public void setPWin(int i) { partieWin = i;}
+
+    /**
+     * Méthode setPLoos 
+     * Elle permet de définir le nombre de partie perdu par le joueur
+     * @param i int qui est mis à jour par PartieLoosUp()
+     */
     public void setPLoos(int i) { partieLoos = i;}
 
+
+    /**
+     * Méthode enregistrement
+     * ELle permet de vérifier si le pseudo saisit est déjà utilisé et de stocker les informations saisis dans un fichier
+     */
     public void enregistrement() {
         if (!Joueur.listEnregistrement().isEmpty()) {
             if (Joueur.listEnregistrement().contains(this)) {
@@ -64,6 +154,12 @@ public class Joueur implements iPlayer {
         }
     }
 
+
+    /**
+     * Méthode updateWin
+     * Elle permet de mettre à jour le compteur de nombre de partie gagné ou perdu pour le joueur
+     * @param aWin boolean qui correspond à la victoire de la partie par le joueur
+     */
     public void updateWin(boolean aWin) {
         List<Joueur> listEnr = Joueur.listEnregistrement();
 
@@ -87,6 +183,12 @@ public class Joueur implements iPlayer {
         }
     }
 
+
+    /**
+     * Méthode equals
+     * Elle permet tester l'égalité entre deux objets
+     * @return le boolean qui test si le pseudo et mot de passe saisit correspondent 
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -96,12 +198,20 @@ public class Joueur implements iPlayer {
                 Objects.equals(mdp, joueur.mdp);
     }
 
+
+    /**
+     * Méthode toString
+     * ELle permet de rassembler toutes les informations du joueur
+     * @return un string qui correspond aux informations du joueur
+     */
     @Override
     public String toString() {
         return pseudo + "," + mdp + "," + partieWin + "," + partieLoos + "\n";
     }
 
 
+
+    
     public static List<Joueur> listEnregistrement() {
         String line = null;
         List<Joueur> lesjoueurs = new ArrayList<>();
@@ -121,11 +231,23 @@ public class Joueur implements iPlayer {
         return lesjoueurs;
     }
 
+
+    /**
+     * Méthode getWinrate
+     * Elle récupère le nombre de partie gagnés et perdue par le joueur pour calculer le winrate du joueur
+     * @return float qui correspond au pourcentage de partie gagné du joueur
+     */
     public float getWinrate() {
         if (partieWin == 0 || partieLoos == 0)
             return 0;
         return partieWin/partieLoos;
     }
+
+    /**
+     * Méthode getClassement
+     * Elle permet de renvoyer le classement du joueur 
+     * @return rank   int qui correspond à la position du joueur dans le classement 
+     */
     public int getClassement() {
         int rank=0;
         return rank;
